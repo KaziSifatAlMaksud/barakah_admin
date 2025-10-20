e90b742fd8d32d7cdda581b14dfa7a61b4662429
Kazi Sifat Al Makusd <kazi.sifat2013@gmail.com>
2025-10-20 16:22:46 +0600

    add php code

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->role === 'admin') {
+            return $next($request);
+        }
+
+        // You can either abort or redirect to login/home
+        abort(403, 'Unauthorized Access - Admins only');
+        // OR: return redirect('/'); 
+    }
+}
